538443879fd8f88eb9dc5ec3299a5e8f3ec4111d
Yurii Skarbek <yurii.skarbek@gmail.com>
2018-10-13 18:23:04 +0300

    Added validation for name, email, password in register form

--- scripts/loginForm.js
+++ scripts/loginForm.js
@@ -1,3 +1,4 @@
+//toogle button for switch between login/register form.
 function formToggle(){
     var login = document.getElementById('login');
     var register = document.getElementById('register');
@@ -14,3 +15,59 @@ function formToggle(){
     }
     
 }
+
+//validation register form
+function registerValidate(){
+    //validate input name with id='register_name'
+    var nameValue = document.getElementById('register_name').value;
+    var nameRGEX = /^[A-Za-z]{2,14}$/;
+    var nameResult = nameRGEX.test(nameValue);
+
+    if(nameResult == false){
+        console.log("name: "+nameResult );
+        document.getElementById('register_name').style.background = 'rgb(255, 179, 179)';
+        document.getElementById('register_name').style.borderLeft = '2px solid rgb(255, 179, 179)';
+        return false;
+    }
+    else{
+        document.getElementById('register_name').style.background = 'white';
+        document.getElementById('register_name').style.borderLeft = 'white';
+    }
+
+    //validate input email with id='register_email'
+    var emailValue = document.getElementById('register_email').value;
+    var emailRGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
+    var emailResult = emailRGEX.test(emailValue);
+
+    if(emailResult == false){
+        console.log("email: "+emailResult );
+        document.getElementById('register_email').style.background = 'rgb(255, 179, 179)';
+        document.getElementById('register_email').style.borderLeft = '2px solid rgb(255, 179, 179)';
+        return false;
+    }
+    else{
+        document.getElementById('register_email').style.background = 'white';
+        document.getElementById('register_email').style.borderLeft = 'white';
+    }
+    //validate input password with id='register_password'
+    var passwordValue = document.getElementById('register_password').value;
+    var passwordRGEX = /^.{5,256}$/;
+    var passwordResult = passwordRGEX.test(passwordValue);
+
+    if(passwordResult == false){
+        console.log("password: "+passwordResult );
+        document.getElementById('register_password').style.background = 'rgb(255, 179, 179)';
+        document.getElementById('register_password').style.borderLeft = '2px solid rgb(255, 179, 179)';
+        return false;
+    }
+    else{
+        document.getElementById('register_password').style.background = 'white';
+        document.getElementById('register_password').style.borderLeft = 'white';
+    }
+    //if validation OK:
+    alert('WELCOME');
+    document.getElementById('register_name').value = '';
+    document.getElementById('register_email').value = '';
+    document.getElementById('register_password').value = '';
+}
+
